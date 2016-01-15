ef9b4ae8addd647db932a62e7821c324281261ad
Farooq AR <farooqbckk@gmail.com>
2016-01-15 20:45:18 +0500

    Replaced Roboto fonts with single Roboto-Regular font

--- app/src/main/java/com/example/stranger/me/typeface/Roboto.java
+++ app/src/main/java/com/example/stranger/me/typeface/Roboto.java
@@ -7,32 +7,11 @@ import android.graphics.Typeface;
  * Created by Farooq on 12/29/2015.
  */
 public class Roboto {
-    private static Typeface robotoLight;
-    private static Typeface robotoMedium;
-    private static Typeface robotoBold;
+    private static Typeface roboto;
 
     public static Typeface getFont(Context context, String style) {
-        if (style != null) {
-            switch (style) {
-                case "normal":
-                    if (robotoMedium == null)
-                        robotoMedium = Typeface.createFromAsset(context.getAssets(), "font/Roboto-Medium.ttf");
-                    return robotoMedium;
-                case "bold":
-                    if (robotoBold == null)
-                        robotoBold = Typeface.createFromAsset(context.getAssets(), "font/Roboto-Bold.ttf");
-                    return robotoBold;
-                default:
-                    if (robotoLight == null)
-                        robotoLight = Typeface.createFromAsset(context.getAssets(), "font/Roboto-Light.ttf");
-                    return robotoLight;
-            }
-        }
-        else {
-            if (robotoLight == null)
-                robotoLight = Typeface.createFromAsset(context.getAssets(), "font/Roboto-Light.ttf");
-            return robotoLight;
-        }
+        roboto = Typeface.createFromAsset(context.getAssets(), "font/Roboto-Regular.ttf");
+        return roboto;
     }
 
 }
