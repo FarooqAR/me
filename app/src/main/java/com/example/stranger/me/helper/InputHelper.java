8bb8d5507de7bfa0b36749849d0cced07d811a1b
Farooq AR <farooqbckk@gmail.com>
2016-01-15 21:54:26 +0500

    added method isValidEmailPassword

--- app/src/main/java/com/example/stranger/me/helper/InputHelper.java
+++ app/src/main/java/com/example/stranger/me/helper/InputHelper.java
@@ -10,21 +10,37 @@ public class InputHelper {
     public static boolean isEmailValid(View v, CharSequence email) {
         boolean isValid = android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
         if(!isValid){
-            SnackbarHelper.create(v,"Email is not valid");
+            SnackbarHelper.create(v,"Email is not valid").show();
         }
         return isValid;
     }
     public static boolean isPasswordValid(View v,String pass){
         if(pass.length()<6){
-            SnackbarHelper.create(v, "Invalid Password: Minimum 6 characters allowed");
+            SnackbarHelper.create(v, "Invalid Password: Minimum 6 characters allowed").show();
             return false;
         } else if(pass.length()>16){
-            SnackbarHelper.create(v,"Invalid Password: Maximum 16 characters allowed");
+            SnackbarHelper.create(v,"Invalid Password: Maximum 16 characters allowed").show();
             return false;
         } else if (!(pass.matches("^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$"))){
-            SnackbarHelper.create(v, "Invalid Password: Must contain alphabets & numbers");
+            SnackbarHelper.create(v, "Invalid Password: Must contain alphabets & numbers").show();
             return false;
         }
         return true;
+    }
+    public static boolean isVaildEmailPassword(View v, String e, String p) {
+        if (e.equals("") && p.equals("")) {
+            SnackbarHelper.create(v, "Email & Password Required").show();
+            return false;
+        } else if (e.equals("")) {
+            SnackbarHelper.create(v, "Email Required").show();
+            return false;
+        } else if (p.equals("")) {
+            SnackbarHelper.create(v, "Password Required").show();
+            return false;
+        } else if (InputHelper.isEmailValid(v, e) && InputHelper.isPasswordValid(v, p)) {
+            return true;
+
+        }
+        return false;
     }
 }
