427733b9e66ec5fad02f50bc7d851a175501d66f
Farooq AR <farooqbckk@gmail.com>
2016-01-15 21:56:18 +0500

    now user can find contacts and perform appropriate actions

---
+++ app/src/main/java/com/example/stranger/me/adapter/ContactListAdapter.java
@@ -0,0 +1,233 @@
+package com.example.stranger.me.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.stranger.me.R;
+import com.example.stranger.me.helper.FirebaseHelper;
+import com.example.stranger.me.modal.User;
+import com.example.stranger.me.widget.CircleImageView;
+import com.example.stranger.me.widget.RobotoTextView;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Farooq on 1/14/2016.
+ */
+public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.ViewHolder> {
+    private ArrayList<User> mUsers;
+    public static Context mContext;
+
+    public ContactListAdapter(Context context, ArrayList<User> users) {
+        mContext = context;
+        mUsers = users;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_contact, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        final User user = mUsers.get(position);
+        holder.displayName.setText(user.getFirstName() + " " + user.getLastName());
+        Picasso.with(mContext).load(user.getProfileImageURL()).placeholder(R.drawable.ic_account_circle).into(holder.profileImage);
+        if (user.getCountry() != null)
+            holder.country.setText(user.getCountry());
+        if (FirebaseHelper.isFriend(user.getId())) {
+            holder.enableUnFriend();
+            holder.unFriend.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    holder.showUnfriendPopup(v, user.getId());
+                }
+            });
+        } else if (FirebaseHelper.isRequested(user.getId())) {
+            holder.enableConfirmAsFriend();
+            holder.confirmAsFriend.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    confirmAsFriend(holder, user.getId());
+                }
+            });
+        } else if (FirebaseHelper.isRequestSent(user.getId())) {
+            holder.enableRequestSent();
+        } else {
+            holder.enableAddAsFriend();
+            holder.addAsFriend.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {sendFriendRequest(holder,user.getId());}
+            });
+        }
+
+    }
+
+    //accept user request
+    public void confirmAsFriend(final ViewHolder holder, final String id) {
+        holder.showConfirmRequestProgress();
+        FirebaseHelper.confirmAsFriend(id, new Firebase.CompletionListener() {
+            @Override
+            public void onComplete(FirebaseError firebaseError, Firebase firebase) {
+                holder.hideConfirmRequestProgress();
+                holder.enableUnFriend();
+            }
+        });
+    }
+
+    //add as friend. this will send request to user
+    public void sendFriendRequest(final ViewHolder holder, String id) {
+        holder.showAddFriendProgress();
+
+        FirebaseHelper.sendFriendRequest(id, new Firebase.CompletionListener() {
+            @Override
+            public void onComplete(FirebaseError firebaseError, Firebase firebase) {
+                if (firebaseError == null) {
+                    holder.hideAddFriendProgress();
+                    holder.enableRequestSent();
+                }
+            }
+        });
+    }
+
+    public void unFriend(final ViewHolder holder, String id) {
+        holder.showUnFriendProgress();
+        //first remove the user from friends list
+        FirebaseHelper.unFriend(id, new Firebase.CompletionListener() {
+            @Override
+            public void onComplete(FirebaseError firebaseError, Firebase firebase) {
+                if (firebase == null) {
+                    holder.hideUnFriendProgress();
+                    holder.enableAddAsFriend();
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public RobotoTextView displayName;
+        public RobotoTextView country;
+        public CircleImageView profileImage;
+        public Button addAsFriend;
+        public Button confirmAsFriend;
+        public Button unFriend;
+        public TextView requestSent;
+        public ProgressBar addAsFriendProgress;
+        public ProgressBar confirmAsFriendProgress;
+        public ProgressBar unFriendProgress;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            displayName = (RobotoTextView) itemView.findViewById(R.id.item_contact_display_name);
+            country = (RobotoTextView) itemView.findViewById(R.id.item_contact_country);
+            profileImage = (CircleImageView) itemView.findViewById(R.id.item_contact_profile_image);
+            addAsFriend = (Button) itemView.findViewById(R.id.action_send_friend_request);
+            confirmAsFriend = (Button) itemView.findViewById(R.id.action_confirm_friend);
+            unFriend = (Button) itemView.findViewById(R.id.action_unfriend);
+            requestSent = (TextView) itemView.findViewById(R.id.friend_request_sent);
+            addAsFriendProgress = (ProgressBar) itemView.findViewById(R.id.action_send_friend_request_progress);
+            confirmAsFriendProgress = (ProgressBar) itemView.findViewById(R.id.action_confirm_friend_progress);
+            unFriendProgress = (ProgressBar) itemView.findViewById(R.id.action_unfriend_progress);
+        }
+
+        public void showUnFriendProgress() {
+            unFriend.setEnabled(false);
+            unFriendProgress.setVisibility(View.VISIBLE);
+        }
+
+        public void hideUnFriendProgress() {
+            unFriend.setEnabled(true);
+            unFriendProgress.setVisibility(View.GONE);
+        }
+
+        public void enableUnFriend() {
+            unFriend.setVisibility(View.VISIBLE);
+            addAsFriend.setVisibility(View.GONE);
+            confirmAsFriend.setVisibility(View.GONE);
+            requestSent.setVisibility(View.GONE);
+        }
+
+        public void showAddFriendProgress() {
+            addAsFriend.setEnabled(false);
+            addAsFriendProgress.setVisibility(View.VISIBLE);
+        }
+
+        public void hideAddFriendProgress() {
+            addAsFriend.setEnabled(true);
+            addAsFriendProgress.setVisibility(View.GONE);
+        }
+
+        public void enableAddAsFriend() {
+            unFriend.setVisibility(View.GONE);
+            addAsFriend.setVisibility(View.VISIBLE);
+            confirmAsFriend.setVisibility(View.GONE);
+            requestSent.setVisibility(View.GONE);
+        }
+
+        public void enableRequestSent() {
+            unFriend.setVisibility(View.GONE);
+            addAsFriend.setVisibility(View.GONE);
+            confirmAsFriend.setVisibility(View.GONE);
+            requestSent.setVisibility(View.VISIBLE);
+        }
+
+        public void showConfirmRequestProgress() {
+            confirmAsFriend.setEnabled(false);
+            confirmAsFriendProgress.setVisibility(View.VISIBLE);
+        }
+
+        public void hideConfirmRequestProgress() {
+            confirmAsFriend.setEnabled(true);
+            confirmAsFriendProgress.setVisibility(View.GONE);
+        }
+
+        public void enableConfirmAsFriend() {
+            unFriend.setVisibility(View.GONE);
+            addAsFriend.setVisibility(View.GONE);
+            confirmAsFriend.setVisibility(View.VISIBLE);
+            requestSent.setVisibility(View.GONE);
+        }
+
+        public void showUnfriendPopup(View v, final String id) {
+            PopupMenu popupMenu = new PopupMenu(mContext, v);
+            popupMenu.inflate(R.menu.action_unfriend);
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    if (item.getItemId() == R.id.action_unfriend) {
+                        FirebaseHelper.unFriend(id, new Firebase.CompletionListener() {
+                            @Override
+                            public void onComplete(FirebaseError firebaseError, Firebase firebase) {
+                                if (firebaseError == null)
+                                    enableAddAsFriend();
+                            }
+                        });
+
+                        return true;
+                    }
+
+                    return false;
+                }
+            });
+            popupMenu.show();
+        }
+    }
+}
--- app/src/main/java/com/example/stranger/me/fragment/FindContactFragment.java
+++ app/src/main/java/com/example/stranger/me/fragment/FindContactFragment.java
@@ -2,18 +2,36 @@ package com.example.stranger.me.fragment;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
 
 import com.example.stranger.me.R;
+import com.example.stranger.me.adapter.ContactListAdapter;
+import com.example.stranger.me.helper.FirebaseHelper;
+import com.example.stranger.me.modal.User;
+import com.example.stranger.me.widget.RobotoEditText;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 
 public class FindContactFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+    private RecyclerView mRecyclerView;
+    private ArrayList<User> mUsers;
+    private ContactListAdapter mAdapter;
+    private RobotoEditText mFindContact;
+    private ImageButton mFindContactBtn;
+    private ProgressBar mProgress;
 
     public FindContactFragment() {
         // Required empty public constructor
@@ -26,6 +44,7 @@ public class FindContactFragment extends Fragment {
         return fragment;
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,9 +55,43 @@ public class FindContactFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_find_contact, container, false);
+        View view = inflater.inflate(R.layout.fragment_find_contact, container, false);
+
+        init(view);
+        mUsers = new ArrayList<User>();
+        mAdapter = new ContactListAdapter(getActivity(), mUsers);
+        LinearLayoutManager manager = new LinearLayoutManager(getActivity());
+        manager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(manager);
+        mFindContactBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = String.valueOf(mFindContact.getText());
+
+                mUsers = new ArrayList<User>();
+                mAdapter = new ContactListAdapter(getActivity(), mUsers);
+                mRecyclerView.setAdapter(mAdapter);
+
+                if (text != null && !text.equals("")) {
+                    mProgress.setVisibility(View.VISIBLE);
+                    String[] textSplitted = text.split(" ");
+                    if (textSplitted.length > 1) {
+                        new ContactAddTask().execute(textSplitted[0], textSplitted[1]);
+                    } else {
+                        new ContactAddTask().execute(textSplitted[0], "?/");
+                    }
+                }
+            }
+        });
+        return view;
     }
 
+    private void init(View view) {
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.find_contact_recyclerview);
+        mFindContact = (RobotoEditText) view.findViewById(R.id.find_contact_edittext);
+        mFindContactBtn = (ImageButton) view.findViewById(R.id.action_find_contact);
+        mProgress = (ProgressBar) view.findViewById(R.id.find_contact_progress);
+    }
 
 
     @Override
@@ -62,4 +115,57 @@ public class FindContactFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+    //find user and add it to recyclerview
+    public class ContactAddTask extends AsyncTask<String, Void, Integer> {
+
+        @Override
+        protected Integer doInBackground(String... text) {
+            String text1 = text[0].toLowerCase();
+            String text2 = text[1].toLowerCase();
+            Map<String, Object> map = (Map<String, Object>) FirebaseHelper.getUsers().getValue();
+            for (Map.Entry<String, Object> entry : map.entrySet()) {
+                User user = null;
+                String user_id = entry.getKey();
+                Map<String, Object> userData = (Map<String, Object>) entry.getValue();
+                String first_name = (userData.get("firstName") != null) ? ((String) userData.get("firstName")) : "";
+                String last_name = (userData.get("lastName") != null) ? ((String) userData.get("lastName")) : "";
+                String country = (userData.get("country") != null) ? ((String) userData.get("country")) : "";
+                String profileImage = (userData.get("profileImageURL") != null) ? ((String) userData.get("profileImageURL")) : "";
+
+                if (first_name.toLowerCase().contains(text1) || last_name.toLowerCase().contains(text1) ||
+                        first_name.toLowerCase().contains(text2) || last_name.toLowerCase().contains(text2)) {
+                    user = new User(first_name, last_name, country, profileImage);
+                    user.setId(user_id);
+                    if(!user_id.equals(FirebaseHelper.getAuthId()))//add contacts other than authenticated one
+                    mUsers.add(user);
+                }
+            }
+            return mUsers.size();
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+            mAdapter.notifyDataSetChanged();
+            mProgress.setVisibility(View.GONE);
+        }
+    }
+
+    public class ContactClearTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            mUsers.clear();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+
 }
