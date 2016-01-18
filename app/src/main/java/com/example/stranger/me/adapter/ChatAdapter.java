6065854a79c2dcfbdb1817e7b9388b2f481b09ab
Farooq AR <farooqbckk@gmail.com>
2016-01-18 18:39:47 +0500

    User can now send messages(only) to his/her friends

--- app/src/main/java/com/example/stranger/me/adapter/ChatAdapter.java
+++ app/src/main/java/com/example/stranger/me/adapter/ChatAdapter.java
@@ -1,42 +1,72 @@
 package com.example.stranger.me.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.stranger.me.R;
 import com.example.stranger.me.helper.FirebaseHelper;
 import com.example.stranger.me.modal.Message;
 import com.example.stranger.me.widget.CircleImageView;
 import com.example.stranger.me.widget.RobotoTextView;
+import com.squareup.picasso.Picasso;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Created by Farooq on 1/2/2016.
  */
 public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
     private ArrayList<Message> mMessages;
-    public ChatAdapter(ArrayList<Message> messages) {
+    public Context mContext;
+
+    public ChatAdapter(Context context, ArrayList<Message> messages) {
         mMessages = messages;
+        mContext = context;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = null;
-        if(viewType==0){
-            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.chat_item_right, parent,false);
-        }
-        else if(viewType == 1){
-            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.chat_item, parent,false);
+        if (viewType == 0) {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.chat_item_right, parent, false);
+        } else if (viewType == 1) {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.chat_item, parent, false);
         }
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        //set views
+        Message message = mMessages.get(position);
+        if (message.getMessage() != null) {
+            holder.mMessageText.setText(message.getMessage());
+        } else if (message.getImageUrl() != null) {
+            Picasso.with(mContext).load(message.getImageUrl()).into(holder.mChatImage);
+        }
+
+        holder.mMessageDate.setText(getFormattedDate(message.getTimestamp()));
+        Picasso.with(mContext).load(FirebaseHelper.getProfileImage(message.getSender())).into(holder.mSenderProfileImage);
+    }
+
+    public String getFormattedDate(String time) {
+        long currentTime = System.currentTimeMillis();
+        long timestamp = Long.parseLong(time);
+        SimpleDateFormat simpleDateFormat = null;
+        if ((currentTime - timestamp) > (1000 * 60 * 60 * 24)) {//that means more than one day has passed
+            simpleDateFormat = new SimpleDateFormat("EEE, hh:mm a");
+        } else {
+            simpleDateFormat = new SimpleDateFormat("hh:mm a");
+        }
+        simpleDateFormat.setTimeZone(TimeZone.getDefault());
+        String formattedDate = simpleDateFormat.format(new Date(timestamp));
+        return formattedDate;
     }
 
     @Override
@@ -50,19 +80,22 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
         return (message.getSender().equals(FirebaseHelper.getAuthId())) ? 0 : 1;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
         public CircleImageView mSenderProfileImage;
         public RobotoTextView mMessageText;
         public RobotoTextView mMessageDate;
+        public ImageView mChatImage;
 
         public ViewHolder(View itemView) {
             super(itemView);
             init(itemView);
         }
-        public void init(View view){
+
+        public void init(View view) {
             mSenderProfileImage = (CircleImageView) view.findViewById(R.id.chat_msg_sender_image);
             mMessageText = (RobotoTextView) view.findViewById(R.id.chat_msg_text);
             mMessageDate = (RobotoTextView) view.findViewById(R.id.chat_msg_date);
+            mChatImage = (ImageView) view.findViewById(R.id.chat_image);
         }
     }
 }
--- app/src/main/java/com/example/stranger/me/fragment/ChatFragment.java
+++ app/src/main/java/com/example/stranger/me/fragment/ChatFragment.java
@@ -4,39 +4,64 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
 import com.example.stranger.me.R;
+import com.example.stranger.me.activity.HomeActivity.PrivateChatListener;
 import com.example.stranger.me.adapter.ChatAdapter;
 import com.example.stranger.me.adapter.ChatFriendListAdapter;
+import com.example.stranger.me.helper.ChatHelper;
 import com.example.stranger.me.helper.FirebaseHelper;
+import com.example.stranger.me.helper.SharedPreferenceHelper;
+import com.example.stranger.me.modal.Message;
 import com.example.stranger.me.modal.User;
+import com.example.stranger.me.widget.RobotoEditText;
+import com.example.stranger.me.widget.RobotoTextView;
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
-public class ChatFragment extends Fragment {
+public class ChatFragment extends Fragment implements PrivateChatListener {
 
     private static final String TAG = "ChatFragment";
+    private static final String CURRENT_USER = "chat_current_user";
     private RecyclerView mRecyclerView;
     private ListView mFriendsListView;
     private RelativeLayout mMainContent;
-    private LinearLayout mRootView;
-    private ArrayList<User> mUsers;
+    private CoordinatorLayout mRootView;
+    private RobotoEditText mChatMsgEditText;
+    private RobotoTextView mChatMsgLengthView;
+    private ImageButton mChatMsgSendBtn;
+    private ImageButton mChatMsgImageBtn;
+    private ImageButton mChatMsgFaceBtn;
 
+    private int mChatMsgLength;
+    private int mChatMsgLengthLeft;
+    private int mChatMsgMaxLength = 150;
+    private ArrayList<User> mUsers;
+    private ArrayList<Message> mMessages;
     private ChatAdapter mChatAdapter;
     private ChatFriendListAdapter mChatFriendListAdapter;
     private OnFragmentInteractionListener mListener;
+    private SharedPreferenceHelper helper = SharedPreferenceHelper.getInstance();
+    private String mCurrentUser;
     private ChildEventListener mUsersDataChangeListener = new ChildEventListener() {
         @Override
         public void onChildAdded(DataSnapshot dataSnapshot, String s) {
@@ -45,14 +70,14 @@ public class ChatFragment extends Fragment {
 
         @Override
         public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-            if(FirebaseHelper.isFriend(dataSnapshot.getKey()))
+            if (FirebaseHelper.isFriend(dataSnapshot.getKey()))
                 new ChangeUserInChatFriendListTask().execute(dataSnapshot);
 
         }
 
         @Override
         public void onChildRemoved(DataSnapshot dataSnapshot) {
-            if(FirebaseHelper.isFriend(dataSnapshot.getKey()))
+            if (FirebaseHelper.isFriend(dataSnapshot.getKey()))
                 new RemoveUserFromChatFriendListTask().execute(dataSnapshot);
         }
 
@@ -64,6 +89,90 @@ public class ChatFragment extends Fragment {
         @Override
         public void onCancelled(FirebaseError firebaseError) {
 
+        }
+    };
+
+    private AdapterView.OnItemClickListener mFriendsListItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            if (!mCurrentUser.equals(mUsers.get(position).getId()) && dataRetrieved) {//if the user didn't click the same item
+                mMessages.clear();
+                mChatAdapter.notifyDataSetChanged();
+                mCurrentUser = mUsers.get(position).getId();
+                updateChatMessageListener();
+            }
+        }
+    };
+    private TextWatcher mChatEditTextListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            mChatMsgLength = s.length();
+            if (mChatMsgLength <= 0) {
+                mChatMsgSendBtn.setEnabled(false);
+                mChatMsgSendBtn.setImageDrawable(getResources().getDrawable(R.drawable.ic_send_gray_24dp));
+            } else {
+                mChatMsgSendBtn.setEnabled(true);
+                mChatMsgSendBtn.setImageDrawable(getResources().getDrawable(R.drawable.send_btn));
+            }
+            mChatMsgLengthLeft = mChatMsgMaxLength - mChatMsgLength;
+            mChatMsgLengthLeft = (mChatMsgLengthLeft < 0) ? 0 : mChatMsgLengthLeft;
+            mChatMsgLengthView.setText("" + mChatMsgLengthLeft);
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+    private ChildEventListener mChatMessageListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+            new AddMessageToList().execute(dataSnapshot);
+        }
+
+        @Override
+        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+        }
+
+        @Override
+        public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+        }
+
+        @Override
+        public void onCancelled(FirebaseError firebaseError) {
+
+        }
+    };
+    private View.OnClickListener mChatSendBtnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (ChatHelper.getPrivateChatNode() != null) {//if data has been retrieved
+                String msg = String.valueOf(mChatMsgEditText.getText());
+                disableViews();
+                Message message = new Message(msg, FirebaseHelper.getAuthId());
+                ChatHelper.sendMessage(mCurrentUser, message, new Firebase.CompletionListener() {
+                    @Override
+                    public void onComplete(FirebaseError firebaseError, Firebase firebase) {
+                        if (firebaseError != null) {
+                            Log.d(TAG, firebaseError.getMessage());
+                        }
+                        enableViews();
+                    }
+                });
+            }
         }
     };
     private ChildEventListener mFriendsListener = new ChildEventListener() {
@@ -93,6 +202,7 @@ public class ChatFragment extends Fragment {
 
         }
     };
+    private boolean dataRetrieved;
 
     public ChatFragment() {
         // Required empty public constructor
@@ -108,8 +218,11 @@ public class ChatFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         mUsers = new ArrayList<>();
-        mChatFriendListAdapter = new ChatFriendListAdapter(getActivity(),R.layout.chat_friends_list_item,mUsers);
+        mMessages = new ArrayList<>();
+        mChatFriendListAdapter = new ChatFriendListAdapter(getActivity(), R.layout.chat_friends_list_item, mUsers);
+
     }
 
     @Override
@@ -118,10 +231,50 @@ public class ChatFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_chat, container, false);
         init(view);
+
+        mMessages.clear();
+        mChatAdapter = new ChatAdapter(getActivity(), mMessages);
         mFriendsListView.setAdapter(mChatFriendListAdapter);
+
+        if (mCurrentUser != null) {
+            try {
+
+                Integer i = new GetIndexTask().execute(mCurrentUser).get();
+                if (i != null)
+                    mFriendsListView.setItemChecked(i, true);
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+
+            updateChatMessageListener();
+
+        } else {
+            mFriendsListView.setItemChecked(0, true);
+        }
+        mChatMsgEditText.addTextChangedListener(mChatEditTextListener);
+        mFriendsListView.setOnItemClickListener(mFriendsListItemClickListener);
         FirebaseHelper.getRoot().child("users").addChildEventListener(mUsersDataChangeListener);
         FirebaseHelper.getRoot().child("friends").child(FirebaseHelper.getAuthId()).addChildEventListener(mFriendsListener);
+
+
+        mChatMsgSendBtn.setOnClickListener(mChatSendBtnListener);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(mChatAdapter);
         return view;
+    }
+
+    public void updateChatMessageListener() {
+        if (mCurrentUser != null && ChatHelper.getConversationKey(mCurrentUser) != null) {
+            FirebaseHelper.getRoot().child("private_conversation").child(ChatHelper.getConversationKey(mCurrentUser))
+                    .orderByChild("timestamp").limitToLast(30).removeEventListener(mChatMessageListener);
+            FirebaseHelper.getRoot().child("private_conversation").child(ChatHelper.getConversationKey(mCurrentUser))
+                    .orderByChild("timestamp").limitToLast(30).addChildEventListener(mChatMessageListener);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
     }
 
     @Override
@@ -129,11 +282,30 @@ public class ChatFragment extends Fragment {
         super.onStop();
     }
 
+    public void disableViews() {
+        mChatMsgEditText.setEnabled(false);
+        mChatMsgSendBtn.setEnabled(false);
+        mChatMsgFaceBtn.setEnabled(false);
+        mChatMsgImageBtn.setEnabled(false);
+    }
+
+    public void enableViews() {
+        mChatMsgEditText.setEnabled(true);
+        mChatMsgSendBtn.setEnabled(true);
+        mChatMsgFaceBtn.setEnabled(true);
+        mChatMsgImageBtn.setEnabled(true);
+    }
+
     private void init(View view) {
         mRecyclerView = (RecyclerView) view.findViewById(R.id.friends_chat_recyclerview);
         mFriendsListView = (ListView) view.findViewById(R.id.friends_chat_list);
         mMainContent = (RelativeLayout) view.findViewById(R.id.friends_chat_content);
-        mRootView = (LinearLayout) view.findViewById(R.id.root_view);
+        mRootView = (CoordinatorLayout) view.findViewById(R.id.root_view);
+        mChatMsgEditText = (RobotoEditText) view.findViewById(R.id.chat_msg_edittext);
+        mChatMsgLengthView = (RobotoTextView) view.findViewById(R.id.chat_msg_length);
+        mChatMsgSendBtn = (ImageButton) view.findViewById(R.id.chat_msg_send_btn);
+        mChatMsgFaceBtn = (ImageButton) view.findViewById(R.id.chat_msg_face_btn);
+        mChatMsgImageBtn = (ImageButton) view.findViewById(R.id.chat_msg_photo_btn);
     }
 
     @Override
@@ -152,20 +324,25 @@ public class ChatFragment extends Fragment {
         mListener = null;
     }
 
+    @Override
+    public void onPrivateChatDataRetrieved() {
+        dataRetrieved = true;
+    }
+
 
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
-    public class RemoveUserFromChatFriendListTask extends AsyncTask<DataSnapshot,Void,Integer>{
+
+    public class RemoveUserFromChatFriendListTask extends AsyncTask<DataSnapshot, Void, Integer> {
 
         @Override
         protected Integer doInBackground(DataSnapshot... params) {
-            for (int i = 0; i < mUsers.size(); i++){
+            for (int i = 0; i < mUsers.size(); i++) {
                 User user = mUsers.get(i);
-                if(user.getId().equals(params[0].getKey())){
+                if (user.getId().equals(params[0].getKey())) {
                     mUsers.remove(i);
-                    Log.d(TAG, "User removed from list: id=" + user.getId() + ", name=" + user.getFirstName());
                     return i;
                 }
             }
@@ -175,46 +352,47 @@ public class ChatFragment extends Fragment {
         @Override
         protected void onPostExecute(Integer integer) {
             super.onPostExecute(integer);
-            if(integer != null){
+            if (integer != null) {
                 mChatFriendListAdapter.notifyDataSetChanged();
                 Log.d(TAG, "adapter notified [item removed]");
             }
         }
     }
-    public class ChangeUserInChatFriendListTask extends AsyncTask<DataSnapshot,Void,Integer>{
+
+    public class ChangeUserInChatFriendListTask extends AsyncTask<DataSnapshot, Void, Integer> {
 
         @Override
         protected Integer doInBackground(DataSnapshot... params) {
             String id = params[0].getKey();
-            for(int i = 0; i< mUsers.size();i++){
-                User previous_data = mUsers.get(i);
-                User current_data = params[0].getValue(User.class);
-                current_data.setId(id);
-                if(previous_data.getId().equals(id)){
-                    mUsers.set(i,current_data);
-                    Log.d(TAG, "User changed in list: id=" + current_data.getId() + ", name=" + current_data.getFirstName()+", online="+current_data.isOnline());
+            User current_data = params[0].getValue(User.class);//user has changed his data (generally online status)
+            current_data.setId(id);
+            for (int i = 0; i < mUsers.size(); i++) {
+                User previous_data = mUsers.get(i);//user data previously showing on screen
+                if (previous_data.getId().equals(id)) {
+                    mUsers.set(i, current_data);
                     return i;
                 }
             }
             return null;
         }
+
         @Override
         protected void onPostExecute(Integer integer) {
             super.onPostExecute(integer);
-            if(integer != null){
+            if (integer != null) {
                 mChatFriendListAdapter.notifyDataSetChanged();
                 Log.d(TAG, "adapter notified [item changed]");
             }
         }
     }
 
-    private class RemoveFriendFromList extends AsyncTask<String,Void,Integer>{
+    private class RemoveFriendFromList extends AsyncTask<String, Void, Integer> {
         @Override
         protected Integer doInBackground(String... params) {
             String idOfFriendToRemove = params[0];
-            for (int i =0;i<mUsers.size();i++){
+            for (int i = 0; i < mUsers.size(); i++) {
                 User user = mUsers.get(i);
-                if(user.getId().equals(idOfFriendToRemove)){
+                if (user.getId().equals(idOfFriendToRemove)) {
                     mUsers.remove(i);
                     return i;
                 }
@@ -225,35 +403,72 @@ public class ChatFragment extends Fragment {
         @Override
         protected void onPostExecute(Integer integer) {
             super.onPostExecute(integer);
-            if(integer != null){
+            if (integer != null) {
                 mChatFriendListAdapter.notifyDataSetChanged();
             }
         }
     }
 
-    private class AddFriendToChatListTask extends AsyncTask<String,Void,Integer>{
+    private class AddFriendToChatListTask extends AsyncTask<String, Void, Integer> {
         @Override
         protected Integer doInBackground(String... params) {
             String idOfFriendToAdd = params[0];
-            for (int i=0;i<mUsers.size();i++){
+            for (int i = 0; i < mUsers.size(); i++) {
                 //check whether friend is already added
-                if(mUsers.get(i).getId().equals(idOfFriendToAdd)){//if friend is already added
+                if (mUsers.get(i).getId().equals(idOfFriendToAdd)) {//if friend is already added
                     return null;
                 }
             }
             //friend will be added only if the above for loop completed without returning null
             User user = FirebaseHelper.getUsers().child(idOfFriendToAdd).getValue(User.class);
             user.setId(idOfFriendToAdd);
+
             mUsers.add(user);
-            return 0;
+            return mUsers.indexOf(user);
         }
 
         @Override
         protected void onPostExecute(Integer integer) {
             super.onPostExecute(integer);
-            if(integer!=null){
+            if (integer != null) {
+                if (mCurrentUser == null) {
+                    mCurrentUser = mUsers.get(0).getId();
+                    FirebaseHelper.getRoot().child("private_conversation").child(ChatHelper.getConversationKey(mCurrentUser))
+                            .orderByChild("timestamp").limitToLast(30).addChildEventListener(mChatMessageListener);
+                }
                 mChatFriendListAdapter.notifyDataSetChanged();
             }
+        }
+    }
+
+
+    private class AddMessageToList extends AsyncTask<DataSnapshot, Void, Integer> {
+        @Override
+        protected Integer doInBackground(DataSnapshot... params) {
+            Message message = params[0].getValue(Message.class);
+            message.setPush_key(params[0].getKey());
+            mMessages.add(message);
+            return mMessages.indexOf(message);
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+            mChatAdapter.notifyItemInserted(integer);
+            mRecyclerView.scrollToPosition(integer);
+        }
+    }
+
+    private class GetIndexTask extends AsyncTask<String, Void, Integer> {
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            for (int i = 0; i < mUsers.size(); i++) {
+                if (mUsers.get(i).getId().equals(params[0])) {
+                    return i;
+                }
+            }
+            return null;
         }
     }
 }
