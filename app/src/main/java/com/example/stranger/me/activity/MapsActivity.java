4f1dfe194b9f03a68ef933b47af8c2bb99d4212f
Farooq AR <farooqbckk@gmail.com>
2016-01-26 20:48:54 +0500

    updated zoom level

--- app/src/main/java/com/example/stranger/me/activity/MapsActivity.java
+++ app/src/main/java/com/example/stranger/me/activity/MapsActivity.java
@@ -34,15 +34,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -50,6 +41,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker and move the camera
         LatLng latLng = new LatLng(latitude, longitude);
         mMap.addMarker(new MarkerOptions().position(latLng).title(location));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,12));
     }
 }
